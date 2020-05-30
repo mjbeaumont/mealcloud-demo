93f6899965c6b421cb9ce115eda0c8c4c7ddb40c
Matthew Beaumont <matt@beaumontwebdev.com>
2020-05-30 13:28:43 -0400

    refactored using dayjs

--- src/components/MattCalendar.vue
+++ src/components/MattCalendar.vue
@@ -42,50 +42,47 @@
 </template>
 
 <script>
-import {
-  dayNamesShort,
-  monthNames,
-  getDate,
-  daysInMonth,
-  firstDay,
-  weeksInMonth
-} from "@/utils/date-helpers";
+import { dayNamesShort, getDate, weeksInMonth } from "@/utils/date-helpers";
+import * as dayjs from "dayjs";
 export default {
   computed: {
-    activeMonth() {
-      return this.activeDate.getMonth();
+    activeDateMeta() {
+      return this.activeDate
+        ? {
+            month: this.activeDate.month(),
+            year: this.activeDate.year()
+          }
+        : null;
     },
-    activeYear() {
-      return this.activeDate.getFullYear();
+    selectedYearMeta() {
+      return this.selectedDate
+        ? {
+            date: this.selectedDate.date(),
+            month: this.selectedDate.month(),
+            year: this.selectedDate.year()
+          }
+        : null;
     },
     firstDay() {
-      return firstDay({ year: this.activeYear, month: this.activeMonth });
+      return this.activeDate.startOf("month").day();
     },
     daysInMonth() {
-      return daysInMonth({ year: this.activeYear, month: this.activeMonth });
+      return this.activeDate.daysInMonth();
     },
     monthHeading() {
-      return this.activeMonth >= 0
-        ? monthNames[this.activeMonth] + " " + this.activeYear
-        : "";
+      return this.activeDate.format("MMMM YYYY");
     },
     nextClass() {
-      if (this.maxDate) {
-        return this.activeYear < this.maxDate.getFullYear() ||
-          this.activeMonth <= this.maxDate.getMonth() - 1
-          ? "visible"
-          : "invisible";
-      }
-      return "visible";
+      return !this.maximumDate ||
+        this.activeDate.startOf("month") < this.maximumDate.startOf("month")
+        ? "visible"
+        : "invisible";
     },
     prevClass() {
-      if (this.minDate) {
-        return this.activeYear > this.minDate.getFullYear() ||
-          this.activeMonth >= this.minDate.getMonth() + 1
-          ? "visible"
-          : "invisible";
-      }
-      return "visible";
+      return !this.minimumDate ||
+        this.activeDate.startOf("month") > this.minimumDate.startOf("month")
+        ? "visible"
+        : "invisible";
     },
     weeksInMonth() {
       return weeksInMonth({
@@ -95,28 +92,27 @@ export default {
     }
   },
   created() {
-    const date = new Date();
-    date.setDate(1);
-    date.setHours(0, 0, 0, 0);
-    this.activeDate = date;
+    this.activeDate = dayjs().startOf("day");
     if (this.minDate) {
-      this.minDate.setHours(0, 0, 0, 0);
+      this.minimumDate = dayjs(this.minDate).startOf("day");
     }
     if (this.maxDate) {
-      this.maxDate.setHours(0, 0, 0, 0);
+      this.maximumDate = dayjs(this.maxDate).startOf("day");
     }
   },
   data() {
     return {
       headings: dayNamesShort,
       activeDate: null,
+      minimumDate: null,
+      maximumDate: null,
       selectedDate: null
     };
   },
   methods: {
     dayClass(week, day) {
       let className = this.outsideRange(week, day)
-        ? "text-grey-600"
+        ? "text-gray-800"
         : "cursor-pointer";
       if (this.isSelected(week, day)) {
         className += " text-yellow-300";
@@ -133,57 +129,39 @@ export default {
     isSelected(week, day) {
       if (
         !this.selectedDate ||
-        this.selectedDate.getFullYear() !== this.activeYear ||
-        this.selectedDate.getMonth() !== this.activeMonth
+        this.selectedDate.year() !== this.activeDateMeta.year ||
+        this.selectedDate.month() !== this.activeDateMeta.month
       ) {
         return false;
       }
 
       const date = this.getDate({ week, day });
-      return this.selectedDate.getDate() === date;
+      return this.selectedDate.date() === date;
     },
     nav(direction) {
-      const newMonth = this.activeDate.getMonth() + direction;
-      if (
-        (!this.minDate || newMonth >= this.minDate.getMonth()) &&
-        (!this.maxDate || newMonth <= this.maxDate.getMonth())
-      ) {
-        if (newMonth === 12) {
-          this.activeDate.setMonth(0);
-          this.activeDate = new Date(
-            this.activeDate.setFullYear(this.activeDate.getFullYear() + 1)
-          );
-        } else if (newMonth === 0) {
-          this.activeDate.setMonth(12);
-          this.activeDate = new Date(
-            this.activeDate.setFullYear(this.activeDate.getFullYear() - 1)
-          );
-        } else {
-          this.activeDate = new Date(this.activeDate.setMonth(newMonth));
-        }
-      }
+      this.activeDate =
+        direction > 0
+          ? this.activeDate.add(1, "month")
+          : this.activeDate.subtract(1, "month");
     },
     outsideRange(week, day) {
       if (this.minDate || this.maxDate) {
         const date = this.getDate({ week, day });
-        const dateObj = new Date(this.activeYear, this.activeMonth, date);
-        if (
-          this.minDate &&
-          dateObj < this.minDate &&
-          dateObj.getTime() !== this.minDate.getTime()
-        ) {
-          return true;
-        }
-
-        if (this.maxDate && dateObj > this.maxDate) {
-          return true;
-        }
+        const dateObj = dayjs(
+          new Date(this.activeDateMeta.year, this.activeDateMeta.month, date)
+        );
+        return (
+          (this.minDate && dateObj.isBefore(this.minDate)) ||
+          (this.maxDate && dateObj.isAfter(this.maxDate))
+        );
       }
       return false;
     },
     selectDate(week, day) {
       const date = this.getDate({ week, day });
-      this.selectedDate = new Date(this.activeYear, this.activeMonth, date);
+      this.selectedDate = dayjs(
+        new Date(this.activeDateMeta.year, this.activeDateMeta.month, date)
+      );
       this.updateValue();
     },
     updateValue() {
