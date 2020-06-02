2cda070d8e3da0ad45533dd18e304c6b14545bba
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-02 14:07:40 -0400

    wired order type component to vuex store

--- src/views/OrderType.vue
+++ src/views/OrderType.vue
@@ -1,18 +1,18 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center h-screen text-3xl md:text-5xl"
-  >
-    <h2 class="font-bold">
+  <div class="flex flex-col items-center justify-center h-screen text-3xl">
+    <h2 class="font-bold md:text-6xl">
       Would you like to order <span class="md:block">Pickup or Delivery?</span>
     </h2>
-    <div class="mt-6 flex flex-col md:flex-row">
+    <div class="mt-12 flex flex-col md:flex-row md:text-5xl">
       <button
-        class="mc-input mc-input-dark uppercase py-2 px-8 md:px-0 md:w-64 text-center"
+        class="mc-input mc-input-dark font-bold uppercase py-2 px-8 md:px-0 md:w-64 text-center"
+        @click="setType('delivery')"
       >
         Delivery
       </button>
       <button
-        class="mc-input mc-input-dark uppercase py-2 px-8 mt-6 md:px-0 md:mt-0 md:ml-6 md:w-64 text-center"
+        class="mc-input mc-input-dark font-bold uppercase py-2 px-8 mt-6 md:px-0 md:mt-0 md:ml-6 md:w-64 text-center"
+        @click="setType('pickup')"
       >
         Pickup
       </button>
@@ -21,7 +21,16 @@
 </template>
 
 <script>
+import { sync } from "vuex-pathify";
 export default {
+  computed: {
+    type: sync("order/type")
+  },
+  methods: {
+    setType(val) {
+      this.type = val;
+    }
+  },
   name: "OrderType"
 };
 </script>
