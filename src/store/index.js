a0ecfe179793db498d7b596a487b1b0e3705bb20
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-02 13:34:33 -0400

    installed vuex pathify and set up basic vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import pathify, { make } from "vuex-pathify";
 
 Vue.use(Vuex);
 
+const state = () => {
+  return {
+    loading: false,
+    activeComponent: ""
+  };
+};
+
+const mutations = make.mutations(state);
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  mutations,
+  modules: {},
+  strict: process.env.NODE_ENV !== "production",
+  plugins: [pathify.plugin]
 });
