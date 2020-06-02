fe082952b3fdb8d45298998a21c7ee5e0e935a6c
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-02 10:31:56 -0400

    first attempt at adding some structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <MCCalendar></MCCalendar>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import MCCalendar from "@/components/MCCalendar";
+import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 export default {
-  components: { MCCalendar },
   data() {
     return {
-      value: null
+      date: null,
+      time: null,
+      times: [
+        { name: "7:00PM", code: "7:00PM" },
+        { name: "7:15PM", code: "7:15PM" },
+        { name: "7:30PM", code: "7:30PM" },
+        { name: "8:00PM", code: "8:00PM" }
+      ]
     };
   },
   name: "App"
@@ -26,5 +30,12 @@ export default {
   text-align: center;
   background: black;
   color: #fff;
+  padding-bottom: 30em;
+}
+.p-dropdown {
+  @apply w-1/2 mt-16 bg-white bg-opacity-50 text-6xl border-white border;
+  .p-dropdown-trigger {
+    @apply border-l border-white px-8;
+  }
 }
 </style>
