d592d36a8236eebc04dfef6d3cd1fa5bec158de7
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-05 16:51:05 -0400

    extracted subcategory into a separate component

--- src/components/Menu/MenuCategory.vue
+++ src/components/Menu/MenuCategory.vue
@@ -3,18 +3,15 @@
     <h2 class="text-4xl font-bold border-b-4 border-green-400 uppercase mb-4">
       {{ category.name }}
     </h2>
-    <h3
-      v-for="subcategory in subcategories"
-      :key="subcategory.id"
-      class="text-3xl"
-    >
-      {{ subcategory.name }}
-    </h3>
+    <MenuSubCategory :subcategories="subcategories" />
   </div>
 </template>
 
 <script>
+import MenuSubCategory from "./MenuSubCategory";
+
 export default {
+  components: { MenuSubCategory },
   computed: {
     subcategories() {
       return this.$store.get("category/subCategories", this.category.id);
@@ -26,5 +23,3 @@ export default {
   }
 };
 </script>
-
-<style scoped></style>
---
+++ src/components/Menu/MenuSubCategory.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3
+      v-for="subcategory in subcategories"
+      :key="subcategory.id"
+      class="text-3xl mb-4"
+    >
+      {{ subcategory.name }}
+    </h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "MenuSubCategory",
+  props: {
+    subcategories: Array
+  }
+};
+</script>
