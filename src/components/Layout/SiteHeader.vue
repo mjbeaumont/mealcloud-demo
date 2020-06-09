a83345d6d85b750784e15bc2e62b0c443cdbf4af
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-09 13:50:35 -0400

    made top menu fixed width on desktop

--- src/components/Layout/SiteHeader.vue
+++ src/components/Layout/SiteHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-primary h-12 lg:h-16 w-full fixed top-0 left-0 flex flex-wrap">
-    <div class="w-full lg:w-1/2 h-full flex items-center justify-start">
+    <div class="w-full lg:w-1/4 h-full flex items-center justify-start">
       <button
         @click.prevent="toggleMobile"
         class="toggle-mobile ml-4 lg:hidden"
@@ -12,45 +12,45 @@
       /></a>
     </div>
     <div
-      class="w-full lg:w-1/2 items-center justify-end lg:pr-4 lg:pl-16 bg-primary mobile-dropdown"
+      class="w-full lg:w-3/4 items-center justify-end lg:pr-4 lg:pl-16 bg-primary mobile-dropdown"
       :class="mobileNavClass"
     >
       <ul
         role="menubar"
-        class="flex flex-col lg:flex-row w-full justify-center items-center justify-between site-nav uppercase"
+        class="mx-auto flex flex-col items-center lg:mx-0 lg:block site-nav uppercase"
       >
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a
             href="https://www.unconventionaldiner.com/#accordion"
             class="hover:underline"
             >Menus</a
           >
         </li>
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a
             href="https://www.unconventionaldiner.com/#buzz"
             class="hover:underline"
             >Buzz</a
           >
         </li>
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a
             href="https://underlineonaldiner.com/#private-events"
             class="hover:underline"
             >Groups/Events</a
           >
         </li>
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a href="#contact" class="hover:underline">Contact</a>
         </li>
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a
             href="https://trycaviar.app.link/xaAWinyFnY"
             class="lg:border lg:border-white lg:p-2 hover:underline"
             >Delivery</a
           >
         </li>
-        <li role="menuitem" class="py-2">
+        <li role="menuitem" class="lg:inline-block lg:ml-6 py-2">
           <a class="lg:border lg:border-white lg:p-2" href="#">Pickup</a>
         </li>
       </ul>
