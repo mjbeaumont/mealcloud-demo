590ee52bda963f9d159698bf69c0cc07faa1231a
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-16 18:07:14 -0400

    added BS google map

--- src/views/OrderCheckout.vue
+++ src/views/OrderCheckout.vue
@@ -4,9 +4,21 @@
       <div>
         <!-- left side desktop -->
         <h2 class="text-xl font-bold">Review and Complete Your Order</h2>
-        <p class="py-8 border-b border-gray-700">
-          Your order will be picked up from {{ locationName }} {{ dateTime }}
-        </p>
+        <div class="py-8 border-b border-gray-700">
+          <div>
+            Your order will be picked up {{ dateTime }} from {{ orderLocation }}
+          </div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.7805471334314!2d-77.02598888471947!3d38.9061334540343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b7ed493773cf%3A0x45657e8fb1bb9a93!2sUnconventional%20Diner!5e0!3m2!1sen!2sus!4v1592344552971!5m2!1sen!2sus"
+            frameborder="0"
+            style="border:0;"
+            allowfullscreen="false"
+            aria-hidden="false"
+            tabindex="0"
+            class="w-full h-full mt-4"
+          ></iframe>
+        </div>
+
         <div class="py-8 border-b border-gray-700">
           <h3 class="text-lg font-bold mb-4">
             Contact Information
@@ -201,10 +213,10 @@ export default {
       dayjs.extend(Calendar);
       const dateTime = this.$store.get("order/dateTime");
       return dateTime.calendar(null, {
-        sameDay: "[Today at] h:mm A", // The same day ( Today at 2:30 AM )
-        nextDay: "[Tomorrow at] h:mm A", // The next day ( Tomorrow at 2:30 AM )
-        nextWeek: "dddd [at] h:mm A", // The next week ( Sunday at 2:30 AM )
-        sameElse: "dddd MM/DD/YYYY [at] h:mm A" // Everything else ( 7/10/2011 )
+        sameDay: "[today at] h:mm A", // The same day ( Today at 2:30 AM )
+        nextDay: "[tomorrow at] h:mm A", // The next day ( Tomorrow at 2:30 AM )
+        nextWeek: "[on] dddd [at] h:mm A", // The next week ( Sunday at 2:30 AM )
+        sameElse: "[on] dddd MM/DD/YYYY [at] h:mm A" // Everything else ( 7/10/2011 )
       });
     },
     email: sync("order/email"),
@@ -217,8 +229,19 @@ export default {
     instructionsIcon() {
       return this.showInstructions ? ["fas", "minus"] : ["fas", "plus"];
     },
-    locationName: get("order/location@name"),
+    location: get("order/location"),
     name: sync("order/name"),
+    orderLocation() {
+      return (
+        this.location.address +
+        ", " +
+        this.location.city +
+        ", " +
+        this.location.state +
+        " " +
+        this.location.zip
+      );
+    },
     phone: sync("order/phone"),
     products: get("cart/products"),
     tax: get("order/tax"),
