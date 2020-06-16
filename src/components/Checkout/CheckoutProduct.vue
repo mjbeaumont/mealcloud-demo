780fcb81687a10bd7b759296e40b26f3fbda51ce
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-16 15:48:43 -0400

    added product summary to checkout

---
+++ src/components/Checkout/CheckoutProduct.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid w-full product-list">
+    <div class="qty-column flex-shrink-0 self-start">{{ product.qty }} x</div>
+    <div class="name-column flex-grow">
+      {{ product.name }}
+    </div>
+    <div class="price-column self-start flex-shrink-0">
+      {{ subtotal | currency }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    subtotal() {
+      return this.product.qty * this.product.price;
+    }
+  },
+  name: "CheckoutProduct",
+  props: {
+    product: Object
+  }
+};
+</script>
+<style scoped>
+.product-list {
+  grid-template-rows: auto auto auto;
+  grid-template-columns: auto 1fr auto;
+  grid-column-gap: 20px;
+}
+
+.qty-column {
+  grid-row: 2 / auto;
+  grid-column: 1 / auto;
+}
+
+.name-column {
+  flex-basis: 100%;
+  grid-row: 2 / auto;
+  grid-column: 2 / auto;
+}
+
+.remove-link {
+  grid-area: 3 / 2 / auto / auto;
+}
+
+.price-column {
+  grid-row: 2 / auto;
+  grid-column: 3 / auto;
+}
+</style>
--- src/views/OrderCheckout.vue
+++ src/views/OrderCheckout.vue
@@ -1,131 +1,148 @@
 <template>
   <div>
-    <h2 class="text-xl font-bold">Review and Complete Your Order</h2>
-    <p class="py-8 border-b border-gray-700">
-      Your order will be picked up from {{ locationName }} {{ dateTime }}
-    </p>
-    <div class="py-8 border-b border-gray-700">
-      <h3 class="text-lg font-bold mb-4">
-        Contact Information
-      </h3>
-      <div class="mb-2">
+    <div>
+      <!-- left side desktop -->
+      <h2 class="text-xl font-bold">Review and Complete Your Order</h2>
+      <p class="py-8 border-b border-gray-700">
+        Your order will be picked up from {{ locationName }} {{ dateTime }}
+      </p>
+      <div class="py-8 border-b border-gray-700">
+        <h3 class="text-lg font-bold mb-4">
+          Contact Information
+        </h3>
+        <div class="mb-2">
+          <validated-input>
+            <input
+              v-model="name"
+              type="text"
+              placeholder="Name"
+              class="text-sm sm:text-base relative w-full border rounded placeholder-gray-500 text-black focus:border-indigo-400 focus:outline-none p-2"
+            />
+          </validated-input>
+        </div>
+        <div class="mb-2">
+          <validated-input class="mb-2">
+            <input
+              v-model="email"
+              type="text"
+              placeholder="Email Address"
+              class="text-sm sm:text-base relative w-full border rounded placeholder-gray-500 text-black focus:border-indigo-400 focus:outline-none p-2"
+            />
+          </validated-input>
+        </div>
         <validated-input>
           <input
-            v-model="name"
+            v-model="phone"
             type="text"
-            placeholder="Name"
+            placeholder="Phone Number (optional)"
             class="text-sm sm:text-base relative w-full border rounded placeholder-gray-500 text-black focus:border-indigo-400 focus:outline-none p-2"
           />
         </validated-input>
       </div>
-      <div class="mb-2">
-        <validated-input class="mb-2">
-          <input
-            v-model="email"
-            type="text"
-            placeholder="Email Address"
-            class="text-sm sm:text-base relative w-full border rounded placeholder-gray-500 text-black focus:border-indigo-400 focus:outline-none p-2"
-          />
-        </validated-input>
-      </div>
-      <validated-input>
-        <input
-          v-model="phone"
-          type="text"
-          placeholder="Phone Number (optional)"
-          class="text-sm sm:text-base relative w-full border rounded placeholder-gray-500 text-black focus:border-indigo-400 focus:outline-none p-2"
-        />
-      </validated-input>
-    </div>
-    <div class="py-8 border-b border-gray-700">
-      <h3 class="text-lg font-bold mb-4">
-        Order Instructions
-      </h3>
-      <div class="mb-4">
-        <MCCheckbox
-          label="Curbside Pickup"
-          v-model="curbside"
-          class="mb-2"
-        ></MCCheckbox>
-        <MCCheckbox
-          label="No Utensils. Let's Reduce Waste!"
-          v-model="utensils"
-          class="mb-2"
-        ></MCCheckbox>
-        <p class="mb-2 mt-4 font-bold">
-          <a
-            @click.prevent="showInstructions = !showInstructions"
-            class="cursor-pointer"
-            ><font-awesome-icon :icon="instructionsIcon"></font-awesome-icon>
-            Add Special Instructions</a
-          >
-        </p>
-        <textarea
-          class="border border-gray-700 placeholder-gray-500 w-full h-32 p-2"
-          :class="instructionsClass"
-          placeholder="e.g. Please include extra ketchup. "
-          v-model="instructions"
-          tabindex="-1"
-        ></textarea>
-      </div>
-    </div>
-    <div class="py-8 border-b border-gray-700">
-      <h3 class="text-lg font-bold mb-4">
-        Payment Information
-      </h3>
-      <div class="mt-4 mb-6 flex flex-wrap -mx-2">
-        <div class="px-2 w-1/2">
-          <button
-            class="mc-input mc-input-dark w-full py-2 hover:text-black"
-            @click="paymentMethod = 'card'"
-            :class="
-              paymentMethod === 'card'
-                ? 'bg-opacity-75 border border-black'
-                : 'text-gray-700'
-            "
-          >
-            Pay with Card
-          </button>
-        </div>
-        <div class="px-2 w-1/2">
-          <button
-            class="mc-input mc-input-dark w-full py-2 hover:text-black"
-            @click="paymentMethod = 'apple'"
-            :class="
-              paymentMethod === 'apple'
-                ? 'bg-opacity-75 border border-black'
-                : 'text-gray-700'
-            "
-          >
-            Apple Pay
-          </button>
+      <div class="py-8 border-b border-gray-700">
+        <h3 class="text-lg font-bold mb-4">
+          Order Instructions
+        </h3>
+        <div class="mb-4">
+          <MCCheckbox
+            label="Curbside Pickup"
+            v-model="curbside"
+            class="mb-2"
+          ></MCCheckbox>
+          <MCCheckbox
+            label="No Utensils. Let's Reduce Waste!"
+            v-model="utensils"
+            class="mb-2"
+          ></MCCheckbox>
+          <p class="mb-2 mt-4 font-bold">
+            <a
+              @click.prevent="showInstructions = !showInstructions"
+              class="cursor-pointer"
+              ><font-awesome-icon :icon="instructionsIcon"></font-awesome-icon>
+              Add Special Instructions</a
+            >
+          </p>
+          <textarea
+            class="border border-gray-700 placeholder-gray-500 w-full h-32 p-2"
+            :class="instructionsClass"
+            placeholder="e.g. Please include extra ketchup. "
+            v-model="instructions"
+            tabindex="-1"
+          ></textarea>
         </div>
       </div>
-      <div
-        class="bg-white h-10 rounded p-4 text-sm sm:text-base relative"
-        id="card-element"
-        v-if="paymentMethod === 'card'"
-      >
-        <iframe
-          frameborder="0"
-          allowtransparency="true"
-          scrolling="no"
-          class="absolute top-0 left-0 h-10"
-          name="__privateStripeFrame16"
-          allowpaymentrequest="true"
-          src="https://js.stripe.com/v3/elements-inner-card-709c45dfbe4aba3400395e54e4e911b1.html#style[base][color]=%2332325d&amp;style[base][fontSmoothing]=antialiased&amp;style[invalid][color]=%23fa755a&amp;style[invalid][iconColor]=%23fa755a&amp;componentName=card&amp;wait=false&amp;rtl=false&amp;keyMode=test&amp;apiKey=pk_test_TYooMQauvdEDq54NiTphI7jx&amp;origin=https%3A%2F%2Fstripe.com&amp;referrer=https%3A%2F%2Fstripe.com%2Fdocs%2Fpayments%2Faccept-a-payment&amp;controllerId=__privateStripeController12"
-          title="Secure payment input frame"
-          style="border: none !important; margin: 0px !important; padding: 10px !important; width: 1px !important; min-width: 100% !important; overflow: hidden !important; display: block !important; user-select: none !important;"
-        ></iframe>
+      <div class="py-8 border-b border-gray-700">
+        <h3 class="text-lg font-bold mb-4">
+          Payment Information
+        </h3>
+        <div class="mt-4 mb-6 flex flex-wrap -mx-2">
+          <div class="px-2 w-1/2">
+            <button
+              class="mc-input mc-input-dark w-full py-2 hover:text-black"
+              @click="paymentMethod = 'card'"
+              :class="
+                paymentMethod === 'card'
+                  ? 'bg-opacity-75 border border-black'
+                  : 'text-gray-700'
+              "
+            >
+              Pay with Card
+            </button>
+          </div>
+          <div class="px-2 w-1/2">
+            <button
+              class="mc-input mc-input-dark w-full py-2 hover:text-black"
+              @click="paymentMethod = 'apple'"
+              :class="
+                paymentMethod === 'apple'
+                  ? 'bg-opacity-75 border border-black'
+                  : 'text-gray-700'
+              "
+            >
+              Apple Pay
+            </button>
+          </div>
+        </div>
+        <div
+          class="bg-white h-10 rounded p-4 text-sm sm:text-base relative"
+          id="card-element"
+          v-if="paymentMethod === 'card'"
+        >
+          <iframe
+            frameborder="0"
+            allowtransparency="true"
+            scrolling="no"
+            class="absolute top-0 left-0 h-10"
+            name="__privateStripeFrame16"
+            allowpaymentrequest="true"
+            src="https://js.stripe.com/v3/elements-inner-card-709c45dfbe4aba3400395e54e4e911b1.html#style[base][color]=%2332325d&amp;style[base][fontSmoothing]=antialiased&amp;style[invalid][color]=%23fa755a&amp;style[invalid][iconColor]=%23fa755a&amp;componentName=card&amp;wait=false&amp;rtl=false&amp;keyMode=test&amp;apiKey=pk_test_TYooMQauvdEDq54NiTphI7jx&amp;origin=https%3A%2F%2Fstripe.com&amp;referrer=https%3A%2F%2Fstripe.com%2Fdocs%2Fpayments%2Faccept-a-payment&amp;controllerId=__privateStripeController12"
+            title="Secure payment input frame"
+            style="border: none !important; margin: 0px !important; padding: 10px !important; width: 1px !important; min-width: 100% !important; overflow: hidden !important; display: block !important; user-select: none !important;"
+          ></iframe>
+        </div>
+        <div v-if="paymentMethod === 'apple'">
+          <button class="w-full bg-black text-white mt-2">
+            <font-awesome-icon
+              :icon="['fab', 'apple-pay']"
+              size="3x"
+            ></font-awesome-icon>
+          </button>
+        </div>
       </div>
-      <div v-if="paymentMethod === 'apple'">
-        <button class="w-full bg-black text-white mt-2">
-          <font-awesome-icon
-            :icon="['fab', 'apple-pay']"
-            size="3x"
-          ></font-awesome-icon>
-        </button>
+    </div>
+    <div class="pt-4">
+      <!-- Right side desktop -->
+      <h3 class="text-lg font-bold mb-4">
+        Order Summary
+        <font-awesome-icon
+          :icon="['fas', 'edit']"
+          @click="edit"
+        ></font-awesome-icon>
+      </h3>
+      <div class="py-4" v-for="product in products" :key="product.productId">
+        <checkout-product :product="product"></checkout-product>
       </div>
+      <div class="border-t border-gray-700 mt-8"></div>
     </div>
   </div>
 </template>
@@ -136,21 +153,12 @@ import dayjs from "dayjs";
 import Calendar from "dayjs/plugin/calendar";
 import ValidatedInput from "@/components/UI/ValidatedInput/ValidatedInput";
 import MCCheckbox from "@/components/UI/MCCheckbox/MCCheckbox";
+import CheckoutProduct from "@/components/Checkout/CheckoutProduct";
 
 export default {
-  components: { MCCheckbox, ValidatedInput },
+  components: { CheckoutProduct, MCCheckbox, ValidatedInput },
   computed: {
-    email: sync("order/email"),
-    name: sync("order/name"),
-    phone: sync("order/phone"),
-    instructions: sync("order/instructions"),
-    instructionsClass() {
-      return this.showInstructions ? "block" : "hidden";
-    },
-    instructionsIcon() {
-      return this.showInstructions ? ["fas", "minus"] : ["fas", "plus"];
-    },
-    locationName: get("order/location@name"),
+    activeComponent: sync("activeComponent"),
     dateTime() {
       dayjs.extend(Calendar);
       const dateTime = this.$store.get("order/dateTime");
@@ -160,7 +168,19 @@ export default {
         nextWeek: "dddd [at] h:mm A", // The next week ( Sunday at 2:30 AM )
         sameElse: "dddd MM/DD/YYYY [at] h:mm A" // Everything else ( 7/10/2011 )
       });
-    }
+    },
+    email: sync("order/email"),
+    instructions: sync("order/instructions"),
+    instructionsClass() {
+      return this.showInstructions ? "block" : "hidden";
+    },
+    instructionsIcon() {
+      return this.showInstructions ? ["fas", "minus"] : ["fas", "plus"];
+    },
+    locationName: get("order/location@name"),
+    name: sync("order/name"),
+    phone: sync("order/phone"),
+    products: get("cart/products")
   },
   data() {
     return {
@@ -169,6 +189,12 @@ export default {
       utensils: true,
       paymentMethod: "card"
     };
+  },
+  methods: {
+    edit() {
+      this.$store.set("cart/open", false);
+      this.activeComponent = "OrderMenu";
+    }
   },
   name: "OrderCheckout"
 };
