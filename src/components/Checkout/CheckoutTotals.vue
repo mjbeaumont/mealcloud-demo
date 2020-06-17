df0722e983b3767ea0858a16801eb93b313719f4
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-17 11:00:54 -0400

    extracted checkout totals into separate component

---
+++ src/components/Checkout/CheckoutTotals.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pt-8 w-full">
+    <p v-if="tax > 0" class="w-full flex justify-between">
+      <span class="font-bold inline-block pr-8"
+        >Sales Tax ({{ taxRate }}%):</span
+      >
+      {{ tax | currency }}
+    </p>
+    <p v-if="gratuity > 0" class="w-full flex justify-between">
+      <span class="font-bold">Gratuity ({{ gratuityRate }}%):</span>
+      {{ gratuity | currency }}
+    </p>
+    <p class="w-full flex justify-between">
+      <span class="font-bold">Total:</span>
+      {{ total | currency }}
+    </p>
+  </div>
+</template>
+<script>
+import { get } from "vuex-pathify";
+
+export default {
+  computed: {
+    gratuity: get("order/gratuity"),
+    gratuityRate: get("order/gratuityRate"),
+    tax: get("order/tax"),
+    taxRate: get("order/location@taxRate"),
+    total: get("order/total")
+  },
+  name: "CheckoutTotals"
+};
+</script>
--- src/views/OrderCheckout.vue
+++ src/views/OrderCheckout.vue
@@ -16,22 +16,7 @@
       <div class="pt-4 lg:pt-0 lg:w-4/12">
         <!-- Right side desktop -->
         <CheckoutCart></CheckoutCart>
-        <div class="pt-8 w-full">
-          <p v-if="tax > 0" class="w-full flex justify-between">
-            <span class="font-bold inline-block pr-8"
-              >Tax ({{ taxRate }}%):</span
-            >
-            {{ tax | currency }}
-          </p>
-          <p v-if="gratuity > 0" class="w-full flex justify-between">
-            <span class="font-bold">Gratuity ({{ gratuityRate }}%):</span>
-            {{ gratuity | currency }}
-          </p>
-          <p class="w-full flex justify-between">
-            <span class="font-bold">Total:</span>
-            {{ total | currency }}
-          </p>
-        </div>
+        <CheckoutTotals></CheckoutTotals>
         <button
           class="bg-primary hover:bg-secondary transition-colors duration-200 px-8 py-2 text-white text-xl rounded mt-8 font-bold block mx-auto"
           tabindex="-1"
@@ -45,16 +30,18 @@
 </template>
 
 <script>
-import { get, sync } from "vuex-pathify";
+import { sync } from "vuex-pathify";
 import { ValidationObserver } from "vee-validate";
 import CheckoutLocation from "@/components/Checkout/CheckoutLocation";
 import CheckoutContact from "@/components/Checkout/CheckoutContact";
 import CheckoutInstructions from "@/components/Checkout/CheckoutInstructions";
 import CheckoutPayment from "@/components/Checkout/CheckoutPayment";
-import CheckoutCart from "./CheckoutCart";
+import CheckoutCart from "@/components/Checkout/CheckoutCart";
+import CheckoutTotals from "@/components/Checkout/CheckoutTotals";
 
 export default {
   components: {
+    CheckoutTotals,
     CheckoutCart,
     CheckoutPayment,
     CheckoutInstructions,
@@ -63,16 +50,8 @@ export default {
     ValidationObserver
   },
   computed: {
-    activeComponent: sync("activeComponent"),
-    gratuity: get("order/gratuity"),
-    gratuityRate: get("order/gratuityRate"),
-    instructions: sync("order/instructions"),
-
-    tax: get("order/tax"),
-    taxRate: get("order/location@taxRate"),
-    total: get("order/total")
+    activeComponent: sync("activeComponent")
   },
-
   methods: {
     process() {
       alert("Checkout!");
